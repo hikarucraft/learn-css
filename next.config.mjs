192c2e8ca42db367d1e4c57464912d1a80444cf7
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-07-26 16:58:02 +0900

    準備した

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+//const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
+  sassOptions: {
+    //includePaths: [path.join(__dirname, 'src/styles')],
+    //prependData: `@use "${path.resolve(__dirname, 'src/styles/main.scss')}";`,
+    //additionalData: `@import "main";`,
+    prependData: `
+    @use 'src/styles/_variables.scss' as *;
+    `,
+  },
+};
 
 export default nextConfig;
