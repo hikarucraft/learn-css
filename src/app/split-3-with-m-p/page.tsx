dadf9d8fc8995eb41c37d0b01887ca9d92e86192
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-07-28 15:35:18 +0900

    ネガティブマージン

---
+++ src/app/split-3-with-m-p/page.tsx
@@ -0,0 +1,26 @@
+import styles from './page.module.scss';
+const Split3WithMP = () => {
+  return (
+    <div className={styles.container}>
+      <ul>
+        <li>
+          <div></div>
+        </li>
+        <li>
+          <div></div>
+        </li>
+        <li>
+          <div></div>
+        </li>
+        <li>
+          <div></div>
+        </li>
+        <li>
+          <div></div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Split3WithMP;
